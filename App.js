99a7ce4c374ea92324bcc91bba1925f828dd4214
bomshakar <Hefehmzy@gmail.com>
2022-12-29 16:03:17 +0100

    eslint config and native paper init

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
 import { Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import { Provider as PaperProvider, Searchbar } from "react-native-paper";
 
 export default function App() {
+	const [searchQuery, setSearchQuery] = React.useState("");
+
+	const onChangeSearch = (query) => setSearchQuery(query);
+
 	const isAndroid = Platform.OS === "android";
 
 	return (
-		<>
+		<PaperProvider>
 			<SafeAreaView style={styles.container(isAndroid, StatusBar.currentHeight)}>
 				<View style={styles.searchContainer}>
-					<Text>Search</Text>
+					<Searchbar placeholder="Search" onChangeText={onChangeSearch} value={searchQuery} />
 				</View>
 				<View style={styles.listContainer}>
 					<Text>List</Text>
 				</View>
 			</SafeAreaView>
 			<ExpoStatusBar style="auto" />
-		</>
+		</PaperProvider>
 	);
 }
 
