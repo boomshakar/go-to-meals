aa8f5b64e5601bba8837a81667b4ca14acd4b6dd
bomshakar <Hefehmzy@gmail.com>
2022-12-30 10:43:47 +0100

    Adding fonts

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import { Lato_400Regular, useFonts as useLato } from "@expo-google-fonts/lato";
+import { Oswald_400Regular, useFonts as useOswald } from "@expo-google-fonts/oswald";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { Provider as PaperProvider } from "react-native-paper";
@@ -6,6 +8,17 @@ import { ResturantsScreen } from "./src/features/resturants/screens/resturants.s
 import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
+	const [oswaldLoaded] = useOswald({
+		Oswald_400Regular,
+	});
+	const [latoLoaded] = useLato({
+		Lato_400Regular,
+	});
+
+	if (!oswaldLoaded || !latoLoaded) {
+		return null;
+	}
+
 	return (
 		<>
 			<ThemeProvider theme={theme}>
