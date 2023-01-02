6e2ed9b7054fea37cca3d04c844b773e7a5acff0
bomshakar <Hefehmzy@gmail.com>
2023-01-02 18:03:09 +0100

    context completeion && activity indicator

--- src/features/resturants/screens/resturants.screen.js
+++ src/features/resturants/screens/resturants.screen.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { FlatList } from "react-native";
-import { Searchbar } from "react-native-paper";
+import { ActivityIndicator, MD2Colors, Searchbar } from "react-native-paper";
 import styled from "styled-components/native";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
@@ -8,32 +8,48 @@ import { SafeArea } from "../../../utils/safe-area.component";
 import { RestaurantInfoCard } from "../components/resturant-info-card";
 
 const SearchContainer = styled.View`
-  padding: ${(props) => props.theme.space[3]};
+	padding: ${(props) => props.theme.space[3]};
+`;
+
+const Loading = styled(ActivityIndicator)`
+	margin-left: -25px;
+`;
+const LoadingContainer = styled.View`
+	position: absolute;
+	top: 50%;
+	left: 50%;
 `;
 
 const RestaurantList = styled(FlatList).attrs({
-  contentContainerStyle: {
-    padding: 16,
-  },
+	contentContainerStyle: {
+		padding: 16,
+	},
 })``;
 
 export const RestaurantsScreen = () => {
-  const restaurantContext = useContext(RestaurantsContext);
+	const { isLoading, error, restaurants } = useContext(RestaurantsContext);
 
-  return (
-    <SafeArea>
-      <SearchContainer>
-        <Searchbar />
-      </SearchContainer>
-      <RestaurantList
-        data={restaurantContext.restaurants}
-        renderItem={() => (
-          <Spacer position="bottom" size="large">
-            <RestaurantInfoCard />
-          </Spacer>
-        )}
-        keyExtractor={(item) => item}
-      />
-    </SafeArea>
-  );
+	return (
+		<SafeArea>
+			{isLoading && (
+				<LoadingContainer>
+					<Loading size={50} animating={true} color={MD2Colors.blue300} />
+				</LoadingContainer>
+			)}
+			<SearchContainer>
+				<Searchbar />
+			</SearchContainer>
+			<RestaurantList
+				data={restaurants}
+				renderItem={({ item }) => {
+					return (
+						<Spacer position="bottom" size="large">
+							<RestaurantInfoCard restaurant={item} />
+						</Spacer>
+					);
+				}}
+				keyExtractor={(item) => item.name}
+			/>
+		</SafeArea>
+	);
 };
--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,5 +1,5 @@
 import camelize from "camelize";
-import { mocks } from "./mock";
+import { mockImages, mocks } from "./mock";
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
 	return new Promise((resolve, reject) => {
 		const mock = mocks[location];
@@ -12,6 +12,9 @@ export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
 
 export const restaurantsTransform = ({ results = [] }) => {
 	const mappedResults = results.map((restaurant) => {
+		restaurant.photos = restaurant.photos.map((p) => {
+			return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
+		});
 		return {
 			...restaurant,
 			isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
@@ -21,12 +24,3 @@ export const restaurantsTransform = ({ results = [] }) => {
 
 	return camelize(mappedResults);
 };
-
-restaurantsRequest()
-	.then(restaurantsTransform)
-	.then((transformedResponse) => {
-		console.log(transformedResponse);
-	})
-	.catch((err) => {
-		console.log("error");
-	});
